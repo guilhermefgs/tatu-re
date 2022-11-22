dc04fccc48d1d306d16d0dda2851cd522509a92a
christianLeomil <christianleomil@gmail.com>
2022-11-22 16:04:59 +0100

    add get financial data

---
+++ experiment/getFinancialData.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 22 09:19:51 2022
+
+@author: chris
+"""
+
+import yfinance as yf
+
+def getFinancialData(dateStart,dateStop):
+    
+    spyticker = yf.Ticker("SPY")
+    dfFinancial= spyticker.history(period="max", interval="1d", 
+                                   start=dateStart, end=dateStop, 
+                                   auto_adjust=True, rounding=True)
+    return dfFinancial
