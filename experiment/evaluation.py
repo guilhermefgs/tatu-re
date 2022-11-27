8427ea9e5a4ed9261dc1932557243e514c0de9fb
rodrigommorais <rodrimorais27@gmail.com>
2022-11-27 15:16:53 +0100

    Performance comparison with S&P500 portfolio

--- experiment/evaluation.py
+++ experiment/evaluation.py
@@ -4,23 +4,27 @@ from getFinancialData import getFinancialData
 from run_model import decision
 from Portfolio import Portfolio
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # TODO check numbers
 def main():
-    portfolio = Portfolio(1000, 0, 10)
+
     start = datetime(2020,3,1)
     end = datetime(2020,3,15)
     df = getFinancialData(start, end)
 
-
-    delta = end - start # returns timedelta
+    initial_money = 1000
+    portfolio = Portfolio(initial_money, 0, 10)
+    max_units = initial_money // df["Close"].iloc[0]
+    SP500_portfolio = Portfolio(initial_money, 0, 10)
 
     print("df.shape[0]: {}".format(df.shape[0]))
     for i in range(df.shape[0]):
         print("i: {}".format(i))
         day = start + timedelta(days=i)
         print(df["Close"].iloc[i])
+
         portfolio.update_price(df["Close"].iloc[i])
         [label, amount] = decision(df, None, 0, 1000)
         print("{} ${} in S&P".format(label, amount))
@@ -30,11 +34,23 @@ def main():
         elif label == "sell":
             portfolio.sell(n_units)
         portfolio.append_day()
-        print(portfolio.timeseries)
-        print("mean {}".format(np.array(portfolio.timeseries).mean()))
+        print("Your portfolio mean {}".format(np.array(portfolio.timeseries).mean()))
+
+        SP500_portfolio.update_price(df["Close"].iloc[i])
+        if i == 1:
+            SP500_portfolio.buy(max_units)
+        SP500_portfolio.append_day()
+        print("S&P500 portfolio mean {}".format(np.array(SP500_portfolio.timeseries).mean()))
+
+    plt.plot(portfolio.timeseries, "-b", label="Your portfolio")
+    plt.plot(SP500_portfolio.timeseries, "-r", label="S&P500 portfolio")
+    plt.title("Performance comparison")
+    plt.xlabel("Time (days)")
+    plt.legend(loc="lower left")
+    plt.ylim(0, 1500)
+    plt.ylabel("Money ($)")
+    plt.show()
         
-
-
 
 
 if __name__ == "__main__":
