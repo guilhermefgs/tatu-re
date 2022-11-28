93056d90e7f93a33677a5434fd8c9610b8cb0f7d
Fernandes_Gu <aateg@live.com>
2022-11-28 22:32:27 +0100

    update: send email on first decision

--- experiment/main.py
+++ experiment/main.py
@@ -6,11 +6,14 @@ from Portfolio import Portfolio
 import numpy as np
 import matplotlib.pyplot as plt
 from send_email import send_email
+import pathlib
 
 
 #-------------Get List of clients
-dfClients=pd.read_excel("resources/listOfClients.xlsx")
-
+clients = [
+    {"Nome": "Christian", "Email": "christianleomil@gmail.com"},
+    {"Nome": "Gui", "Email": "aateg.german@live.com"}
+]
 
 #-------------Read Financial Data
 start = datetime(2020,3,1)
@@ -23,8 +26,6 @@ max_units = initial_money // df["Close"].iloc[0]
 SP500_portfolio = Portfolio(initial_money, 0, 10)
 
 print("df.shape[0]: {}".format(df.shape[0]))
-cash = []
-sandp = []
 for i in range(df.shape[0]):
     print("i: {}".format(i))
     day = start + timedelta(days=i)
@@ -33,23 +34,21 @@ for i in range(df.shape[0]):
     portfolio.update_price(df["Close"].iloc[i])
     [label, amount] = decision(df, None, portfolio.n_stocks*portfolio.unit_price, portfolio.total)
     print("{} ${} in S&P".format(label, amount))
+    if i == 0:
+        send_email(clients, label, amount)
+        
     n_units = amount // portfolio.unit_price
     if label == "buy":
         portfolio.buy(n_units)
     elif label == "sell":
         portfolio.sell(n_units)
-    cash.append(portfolio.in_cash)
-    sandp.append(portfolio.n_stocks*portfolio.unit_price)
     portfolio.append_day()
     print("Your portfolio mean {}".format(np.array(portfolio.timeseries).mean()))
-    print("{} of portfolio allocated in S&P".format(sandp[-1]))
+
     SP500_portfolio.update_price(df["Close"].iloc[i])
     if i == 1:
         SP500_portfolio.buy(max_units)
     SP500_portfolio.append_day()
     print("S&P500 portfolio mean {}".format(np.array(SP500_portfolio.timeseries).mean()))
 
-#-------------Send Email with action
-amount='1000'
-
-send_email(dfClients, label, amount)
+#send_email(clients, label, amount)
