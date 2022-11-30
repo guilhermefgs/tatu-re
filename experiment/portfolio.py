2a187f2996dceb7d272c5279d71c260ee0965882
Fernandes_Gu <aateg@live.com>
2022-11-30 08:50:34 +0100

    refactor files

--- experiment/portfolio.py
+++ experiment/portfolio.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
@@ -6,7 +7,7 @@ from typing import List
 
 class Portfolio:
 
-    def __init__(self, initial_capital, start_date) -> None:
+    def __init__(self, initial_capital, start_date, benchmark) -> None:
 
         # static variable
         self.initial_capital: float = initial_capital
@@ -22,6 +23,9 @@ class Portfolio:
         self.in_cash_series: List = []
         self.in_asset_series: List = []
 
+        # benchmark
+        self.benchmark = benchmark
+
     def update(self, date, price, quantity, action) -> None:
         self.operations.append(
             {
@@ -34,26 +38,22 @@ class Portfolio:
         amount = price * quantity
         if action == "buy":
             self.in_cash -= amount
-            self.in_asset += amount
             self.n_stocks += quantity
         elif action == "sell":
             self.in_cash += amount
-            self.in_asset -= amount
             self.n_stocks -= quantity
+        self.in_asset = self.n_stocks * price
         self.in_cash_series.append(self.in_cash)
         self.in_asset_series.append(self.in_asset)
 
     def plot(self) -> None:
-        
+        benchmark = self.benchmark.get_timeseries()
         total = np.add(self.in_cash_series, self.in_asset_series)
-        print(total)
-        #benchmark = How to simulate
-        timeindex = [self.start_date - timedelta(days=x) for x in range(len(total))]
+        self.plot_compare_benchmark(benchmark.index, total)
+        self.plot_cash_vs_asset(benchmark.index)
 
-        self.plot_in_cash_vs_asset(timeindex)
-
-    def plot_in_cash_vs_asset(self, timeindex) -> None:
-        plt.figure(1, (16,8))
+    def plot_cash_vs_asset(self, timeindex) -> None:
+        plt.figure(2, (16,8))
         plt.plot(timeindex, self.in_asset_series, "-b", label="In Asset")
         plt.plot(timeindex, self.in_cash_series, "-r", label="In Cash")
         plt.title("Cash flow of client point of view ($)", fontsize=20)
@@ -63,36 +63,34 @@ class Portfolio:
         plt.legend(loc="lower left", fontsize=12)
         plt.grid()
         plt.show()
+    
+    def plot_compare_benchmark(self, timeindex, total):
+        """Function that plots portfolio versus benchmark results in time
+        """
+        plt.figure(1, (16,8))
+        plt.plot(timeindex, total, "-b", label="Your portfolio")
+        plt.plot(timeindex, self.benchmark.get_timeseries(), "-r", label="S&P500 portfolio")
+        plt.title("Portfolio Performance Comparison", fontsize=20)
+        plt.xlabel("Time (days)", fontsize=16)
+        plt.legend(loc="lower left", fontsize=12)
+        plt.ylim(0, 1500)
+        plt.ylabel("Money ($)", fontsize=16)
+        plt.grid()
+        plt.show()
 
+class Benchmark:
 
-    def compare_benchmark(self):
-        pass
+    def __init__(self, initial_capital, start_date, timeseries) -> None:
+        
+        self.initial_capital: float = initial_capital
+        self.start_date: datetime = start_date
 
+        self.timeseries: pd.Series = timeseries
 
-def plot_benchmark_comparison(portfolio, benchmark, time_index) -> None:
-    """Function that plots portfolio versus benchmark results in time
-    :param portfolio: customer portfolio series
-    :param benchmark: benchmark series
-    """
-    plt.figure(1, (16,8))
-    plt.plot(time_index, portfolio, "-b", label="Your portfolio")
-    plt.plot(time_index, benchmark, "-r", label="S&P500 portfolio")
-    plt.title("Performance Comparison", fontsize=20)
-    plt.xlabel("Time (days)", fontsize=16)
-    plt.legend(loc="lower left", fontsize=12)
-    plt.ylim(0, 1500)
-    plt.ylabel("Money ($)", fontsize=16)
-    plt.grid()
-    plt.show()
+    def get_timeseries(self):
+        price_first_day = self.timeseries[0]
+        # initial capital needs to be higher than price
+        max_n_stocks = self.initial_capital // price_first_day
+        cash = self.initial_capital - (price_first_day * max_n_stocks)
 
-def plot_cash(cash, sandp, time_index):
-
-    plt.figure(2, (16,8))
-    plt.plot(time_index, cash, label="Cash")
-    plt.plot(time_index, sandp, label="S&P")
-    plt.title("Cash flow from client point of view ($)", fontsize=20)
-    plt.xlabel("Time (days)", fontsize=16)
-    plt.ylabel("Money ($)", fontsize=16)
-    plt.legend(fontsize=12)
-    plt.grid()
-    plt.show()
+        return (max_n_stocks * self.timeseries) + cash
