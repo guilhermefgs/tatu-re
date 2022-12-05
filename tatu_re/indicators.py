07be809ab49b226ca5f5b27c17751747fe1f3f83
rodrigommorais <rodrimorais27@gmail.com>
2022-12-05 19:11:05 +0100

    Indicators from dictionary to df

--- tatu_re/indicators.py
+++ tatu_re/indicators.py
@@ -1,15 +1,15 @@
 from datetime import timedelta
 from datetime import datetime
-from utils import get_data
 from talib import abstract
+import pandas as pd
 
-def calculate_indicators(prediction_day):
+def calculate_indicators(df):
     """
     prediction_day: date in which the recomendation engine is used (datetime format)
     """
-    train_start = prediction_day - timedelta(days=120) #TODO define better this number
-    train_end = prediction_day
-    df = get_data(train_start, train_end)
+    # train_start = prediction_day - timedelta(days=120) #TODO define better this number
+    # train_end = prediction_day
+    # df = get_data(train_start, train_end)
     df = df.dropna()
 
     inputs = {
@@ -24,8 +24,8 @@ def calculate_indicators(prediction_day):
     indicators = {
 
         # Trend indicators
-        "SMA20": abstract.SMA(inputs, timeperiod=20), # Simple moving avarage over 20 days
-        "SMA50": abstract.SMA(inputs, timeperiod=50), # Simple moving avarage over 50 days
+        "SMA20": abstract.SMA(inputs, timeperiod=20), # Simple moving average over 20 days
+        "SMA50": abstract.SMA(inputs, timeperiod=50), # Simple moving average over 50 days
         "SAR": abstract.SAR(inputs), # Parabolic stop and reverse
 
         # Volume indicators
@@ -44,6 +44,8 @@ def calculate_indicators(prediction_day):
         "ULTOSC ": abstract.ULTOSC(inputs) # Ultimate Oscillator
 
         }
+
+    pd.DataFrame.from_dict(indicators)
     
     return indicators
 
