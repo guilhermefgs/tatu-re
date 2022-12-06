72493b9df4d109e61ffcbd36814b886dffe1934b
Fernandes_Gu <aateg@live.com>
2022-12-06 17:45:46 +0100

    wrong indicators call in main

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ from datetime import datetime
 from tatu_re.portfolio import Portfolio, Benchmark
 from tatu_re.recommendation_engine import Monkey
 from tatu_re.utils import get_data
-from tatu_re.indicators import calculate_indicators
 
 #------------- S&P Data
 start = datetime(2020,3,1)
 end = datetime(2020,3,30)
 df = get_data(start, end)
-
-#------------- Indicators calculation
-indicators = calculate_indicators(start)
 
 #------------- Create Portfolio, Benchmark and Manager
 benchmark = Benchmark(initial_capital=1000, start_date=start, timeseries=df["Close"])
