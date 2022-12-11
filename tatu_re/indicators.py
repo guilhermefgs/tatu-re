001e841c14fc1b0395e4efc24664e63b7c4a8b1d
Rodrigo Mardegam Morais <52223541+rodrigommorais@users.noreply.github.com>
2022-12-11 15:09:07 +0100

    Indicators (#38)
    * first version of indicators calculation
    
    * Indicators from dictionary to df
    
    * wrong indicators call in main
    
    * refactor: switch from talib to ta
    
    Co-authored-by: Fernandes_Gu <aateg@live.com>

---
+++ tatu_re/indicators.py
@@ -0,0 +1,66 @@
+from ta.trend import sma_indicator, psar_up, psar_down
+from ta.volume import on_balance_volume, acc_dist_index
+from ta.volatility import average_true_range, bollinger_mavg
+from ta.momentum import rsi, stoch, ultimate_oscillator
+
+import pandas as pd
+
+def calculate_indicators(df: pd.DataFrame) -> pd.DataFrame:
+    """Function to calculate indicators
+
+    Trend Indicators
+    - SMA20: Simple moving average with 20 days window
+    - SMA50: Simple moving average with 50 days window
+    - PSAR_UP: Parabolic stop and reverse for upward trends
+    - PSAR_DOWN: Parabolic stop and reverse for downward trends
+
+    Volume Indicators
+    - OBV: On balance volume
+    - AD: Accumulation / Distribution indicator
+
+    Volatility Indicators
+    - ATR: Average true range
+    - BBANDS: Bollinger Bands with 20 days simple moving average (MA).
+
+    Momentum Indicators
+    - RSI: Relative Strength Index
+    - STOCHRSI: Stochastic Relative Strength Index
+    - ULTOSC: Ultimate Oscillator
+
+    :param df: DataFrame of the security, given by Yfinance
+    :return: DataFrame with all new series of indicators
+
+    """
+
+    df = df.dropna()
+    high = df["High"]
+    low = df["Low"]
+    close = df["Close"]
+    volume=df["Volume"]
+
+    # Add all ta indicators    
+    indicators = {
+
+        # Trend indicators
+        "SMA20": sma_indicator(close, window=20),
+        "SMA50": sma_indicator(close, window=20),
+        "PSAR_UP": psar_up(high, low, close),
+        "PSAR_DOWN": psar_down(high, low, close),
+        
+        # Volume indicators
+        "OBV": on_balance_volume(close, volume),
+        "AD": acc_dist_index(high, low, close, volume),
+
+        # Volatility indicators
+        "ATR": average_true_range(high, low, close),
+        "BBANDS ": bollinger_mavg(close),
+        
+        # Momentum indicators
+        "RSI": rsi(close),
+        "STOCHRSI": stoch(high, low, close),
+        "ULTOSC ": ultimate_oscillator(high, low, close) 
+
+    }
+    
+    return pd.DataFrame.from_dict(indicators)
+
